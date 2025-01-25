7c846f15af7c90751b31a9263a706adecba1c055
Gregory <gregory.laigneau@protonmail.com>
2025-01-25 10:28:34 +0700

    Update backend port to avoid conflict with frontend

--- app.mjs
+++ app.mjs
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
