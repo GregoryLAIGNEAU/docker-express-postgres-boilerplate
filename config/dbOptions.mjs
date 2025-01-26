66d6f77e01e7c6fb91f5236e0e534d232898936d
Gregory <gregory.laigneau@protonmail.com>
2025-01-26 13:25:43 +0700

    Reorder database options properties to match convention

--- config/dbOptions.mjs
+++ config/dbOptions.mjs
@@ -1,9 +1,9 @@
 const dbOptions = {
   host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   database: process.env.PG_NAME,
   username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
   ssl: process.env.PG_SSL,
 };
 
