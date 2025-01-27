7b6c447fc843e64c4775daf5c7ed3db66d8f2745
Gregory <gregory.laigneau@protonmail.com>
2025-01-27 12:34:36 +0700

    Add function to activate a user account in the database

--- models/userModel.mjs
+++ models/userModel.mjs
@@ -9,10 +9,26 @@ async function createUser(
 ) {
   await sql`
     INSERT INTO users 
-      (firstname, lastname, email, password_hash, activation_token_hash)
+      (firstname, lastname, email, password_hash, activation_token_hash, activation_token_hash_expires_at)
     VALUES 
-      (${firstName}, ${lastName}, ${email}, ${passwordHash}, ${activationTokenHash})
+      (${firstName}, ${lastName}, ${email}, ${passwordHash}, ${activationTokenHash}, NOW() + interval '15 minutes')
   `;
 }
 
-export { createUser };
+async function activateUserAccount(activationTokenHash) {
+  const result = await sql`
+    UPDATE users
+    SET 
+      user_status_id = 2, 
+      activation_token_hash = NULL, 
+      activation_token_hash_expires_at = NULL
+    WHERE 
+      activation_token_hash = ${activationTokenHash} 
+      AND activation_token_hash_expires_at > NOW()
+    RETURNING *;
+  `;
+
+  return result.length > 0;
+}
+
+export { createUser, activateUserAccount };
