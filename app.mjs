0e1e4c27ad5b0ab13b4da969f12437d04e1d160f
Gregory <gregory.laigneau@protonmail.com>
2025-01-29 15:15:28 +0700

    Add jwt strategy to be used with passport

--- app.mjs
+++ app.mjs
@@ -5,12 +5,16 @@ import corsOptions from "./config/corsOptions.mjs";
 import notFoundMiddleware from "./middlewares/notFoundMiddleware.mjs";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.mjs";
 import authRouter from "./routers/authRouter.mjs";
+import passport from "passport";
+import jwtStrategy from "./strategies/jwtStrategy.mjs";
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+passport.use(jwtStrategy);
 
 app.use("/", authRouter);
 
