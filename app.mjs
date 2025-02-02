cbadcd9a44bc185b2e4e439297395d6a1769bd9c
Gregory <gregory.laigneau@protonmail.com>
2025-02-02 23:11:26 +0700

    Add csrf-token route and middleware to protect routes globally

--- app.mjs
+++ app.mjs
@@ -8,14 +8,23 @@ import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.mjs";
 import authRouter from "./routers/authRouter.mjs";
 import passport from "passport";
 import jwtStrategy from "./strategies/jwtStrategy.mjs";
+import cookieParser from "cookie-parser";
+import csrfRouter from "./routers/csrfRouter.mjs";
+import { doubleCsrfProtection } from "./middlewares/csrfMiddleware.mjs";
 
 const app = express();
 
 app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 passport.use(jwtStrategy);
+
+app.use("/csrf-token", csrfRouter);
+
+app.use(doubleCsrfProtection);
 
 app.use("/", authRouter);
 
