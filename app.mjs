81f16b8d9456538afd8e0a517c1ff1ca12050a94
Gregory <gregory.laigneau@protonmail.com>
2025-02-03 10:57:30 +0700

    Add helmet middleware to app.mjs

--- app.mjs
+++ app.mjs
@@ -1,23 +1,25 @@
-import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import express from "express";
+import helmet from "helmet";
 
 import corsOptions from "./config/corsOptions.mjs";
-import logger from "./utils/loggerUtil.mjs";
+import { doubleCsrfProtection } from "./middlewares/csrfMiddleware.mjs";
 import notFoundMiddleware from "./middlewares/notFoundMiddleware.mjs";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.mjs";
-import authRouter from "./routers/authRouter.mjs";
 import passport from "passport";
-import jwtStrategy from "./strategies/jwtStrategy.mjs";
-import cookieParser from "cookie-parser";
+import authRouter from "./routers/authRouter.mjs";
 import csrfRouter from "./routers/csrfRouter.mjs";
-import { doubleCsrfProtection } from "./middlewares/csrfMiddleware.mjs";
+import jwtStrategy from "./strategies/jwtStrategy.mjs";
+import logger from "./utils/loggerUtil.mjs";
 
 const app = express();
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({ limit: "10kb" }));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
 passport.use(jwtStrategy);
