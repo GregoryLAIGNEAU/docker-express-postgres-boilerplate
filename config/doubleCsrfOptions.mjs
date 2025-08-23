7af12e535f89253bbbd3006a046a2bef9da4dc88
Gregory <gregory.laigneau@protonmail.com>
2025-08-23 21:24:56 +0700

    Add getSessionIdentifier

--- config/doubleCsrfOptions.mjs
+++ config/doubleCsrfOptions.mjs
@@ -1,7 +1,14 @@
 import { isProduction } from "../utils/envUtil.mjs";
+import jwt from "jsonwebtoken";
 
 const doubleCsrfOptions = {
   getSecret: () => process.env.CSRF_SECRET,
+  getSessionIdentifier: (req) => {
+    const token = req.cookies?.access_token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    return decoded.sid;
+  },
   cookieName: isProduction ? "__Host-psifi.x-csrf-token" : "x-csrf-token",
   cookieOptions: {
     secure: isProduction,
