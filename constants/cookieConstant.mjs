e7f0fd6844a26b84f9804a36fb39640ef4aa8721
Gregory <gregory.laigneau@protonmail.com>
2025-09-06 01:08:31 +0700

    Move CSRF cookie name to constant

--- constants/cookieConstant.mjs
+++ constants/cookieConstant.mjs
@@ -2,3 +2,4 @@ import { isProduction } from "../utilities/envUtility.mjs";
 
 export const ACCESS_COOKIE_NAME = isProduction ? "__Host-access_token" : "access_token";
 export const REFRESH_COOKIE_NAME = isProduction ? "__Host-refresh_token" : "refresh_token";
+export const X_CSRF_TOKEN_COOKIE_NAME = isProduction ? "__Host-psifi.x-csrf-token" : "x-csrf-token"
