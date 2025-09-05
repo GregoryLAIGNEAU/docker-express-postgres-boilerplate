90864fb4b85a4a93d9d55d40ac3dcc38fe9aab51
Gregory <gregory.laigneau@protonmail.com>
2025-09-06 01:11:07 +0700

    Use access cookie constant for CSRF token in getSessionIdentifier

--- config/doubleCsrfOptions.mjs
+++ config/doubleCsrfOptions.mjs
@@ -1,25 +1,29 @@
 import jwt from "jsonwebtoken";
+import {
+  ACCESS_COOKIE_NAME,
+  X_CSRF_TOKEN_COOKIE_NAME,
+} from "../constants/cookieConstant.mjs";
 import { isProduction } from "../utilities/envUtility.mjs";
 
 const doubleCsrfOptions = {
   getSecret: () => process.env.CSRF_SECRET,
   getSessionIdentifier: (req) => {
-    const token = req.cookies?.access_token
+    const token = req.cookies?.[ACCESS_COOKIE_NAME];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     return decoded.sid;
   },
-  cookieName: isProduction ? "__Host-psifi.x-csrf-token" : "x-csrf-token",
+  cookieName: X_CSRF_TOKEN_COOKIE_NAME,
   cookieOptions: {
     secure: isProduction,
   },
   getTokenFromRequest: (req) => {
-    if (req.is('application/x-www-form-urlencoded')) {
+    if (req.is("application/x-www-form-urlencoded")) {
       return req.body._csrf;
     }
 
-    return req.headers['x-csrf-token'];
-  }
+    return req.headers["x-csrf-token"];
+  },
 };
 
 export default doubleCsrfOptions;
