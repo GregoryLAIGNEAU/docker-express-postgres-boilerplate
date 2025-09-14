41e3b78ba3fc1326be718e72d160f25714728487
Gregory <gregory.laigneau@protonmail.com>
2025-09-15 02:00:39 +0700

    Add endpoint to soft delete user profile

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -1,3 +1,4 @@
+import { ACCOUNT_STATUS } from "../constants/accountStatusConstant.mjs";
 import { getUserById, updateUserById } from "../models/userModel.mjs";
 import { updateUserValidator } from "../validators/userValidator.mjs";
 
@@ -24,4 +25,26 @@ export const updateUser = async (req, res) => {
   }
 
   return res.status(200).json(updatedUser);
+};
+
+export const deleteUser = async (req, res) => {
+  const currentUserId = req.user.id;
+
+  const payload = {
+    account_status_id: ACCOUNT_STATUS.deactivated,
+    deleted_at: new Date(),
+  };
+  const updatedUser = await updateUserById(currentUserId, payload);
+
+  if (!updatedUser) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "User deactivated successfully",
+  });
 };
--- routers/api/v1/userRouter.mjs
+++ routers/api/v1/userRouter.mjs
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getUser, updateUser } from "../../../controllers/userController.mjs";
+import {
+  deleteUser,
+  getUser,
+  updateUser,
+} from "../../../controllers/userController.mjs";
 import jwtAuthMiddleware from "../../../middlewares/jwtAuthMiddleware.mjs";
 
 const userRouter = Router();
@@ -7,5 +11,7 @@ const userRouter = Router();
 userRouter.get("/me", jwtAuthMiddleware, getUser);
 
 userRouter.patch("/me", jwtAuthMiddleware, updateUser);
+
+userRouter.delete("/me", jwtAuthMiddleware, deleteUser);
 
 export default userRouter;
