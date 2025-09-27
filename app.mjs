6e699a5bed213d9a360fce5e31c16e246e630133
Gregory <gregory.laigneau@protonmail.com>
2025-09-27 17:45:12 +0700

    Use alias to import utility

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import passport from "passport";
 
 import corsOptions from "./config/corsOptions.mjs";
 import { doubleCsrfProtection } from "#middlewares/csrfMiddleware.mjs";
@@ -9,12 +10,11 @@ import errorHandlerMiddleware from "#middlewares/errorHandlerMiddleware.mjs";
 import morganMiddleware from "#middlewares/morganMiddleware.mjs";
 import notFoundMiddleware from "#middlewares/notFoundMiddleware.mjs";
 import rateLimiterMiddleware from "#middlewares/rateLimiterMiddleware.mjs";
-import passport from "passport";
 import authRouter from "#routers/api/v1/authRouter.mjs";
 import csrfRouter from "#routers/api/v1/csrfRouter.mjs";
 import jwtStrategy from "./strategies/jwtStrategy.mjs";
-import logger from "./utilities/loggerUtility.mjs";
-import { isProduction } from "./utilities/envUtility.mjs";
+import logger from "#utilities/loggerUtility.mjs";
+import { isProduction } from "#utilities/envUtility.mjs";
 import userRouter from "#routers/api/v1/userRouter.mjs";
 import { csrfErrorHandlerMiddleware } from "#middlewares/csrfErrorHandlerMiddleware.mjs";
 
