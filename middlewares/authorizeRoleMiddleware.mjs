ed96ecfbbd42ce40522205966fedf17a79d6fb28
Gregory <gregory.laigneau@protonmail.com>
2025-10-01 01:54:15 +0700

    Implement role authorization middleware

---
+++ middlewares/authorizeRoleMiddleware.mjs
@@ -0,0 +1,16 @@
+import { ForbiddenError } from "#errors/ForbiddenError.mjs";
+import { UnauthorizedError } from "#errors/UnauthorizedError.mjs";
+
+export const authorizeRole =
+  (...allowedRoles) =>
+  (req, _res, next) => {
+    if (!req.user?.role_id) {
+      throw new UnauthorizedError("Authentication required.");
+    }
+
+    if (!allowedRoles.includes(req.user.role_id)) {
+      throw new ForbiddenError("You do not have permission to access this resource.");
+    }
+
+    next();
+  };
