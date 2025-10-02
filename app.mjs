146b6743f6371b841386a3457744d3e7b1fe8298
Gregory <gregory.laigneau@protonmail.com>
2025-10-03 05:01:59 +0700

    Sort import

--- app.mjs
+++ app.mjs
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import passport from "passport";
 
 import { corsOptions } from "#config/corsOptions.mjs";
+import { csrfErrorHandlerMiddleware } from "#middlewares/csrfErrorHandlerMiddleware.mjs";
 import { doubleCsrfProtection } from "#middlewares/csrfMiddleware.mjs";
 import { errorHandlerMiddleware } from "#middlewares/errorHandlerMiddleware.mjs";
 import { morganMiddleware } from "#middlewares/morganMiddleware.mjs";
@@ -12,11 +13,10 @@ import { notFoundMiddleware } from "#middlewares/notFoundMiddleware.mjs";
 import { rateLimiterMiddleware } from "#middlewares/rateLimiterMiddleware.mjs";
 import { authRouter } from "#routers/api/v1/authRouter.mjs";
 import { csrfRouter } from "#routers/api/v1/csrfRouter.mjs";
-import { jwtStrategy } from "#strategies/jwtStrategy.mjs";
-import { logger } from "#utilities/loggerUtility.mjs";
-import { isProduction } from "#utilities/envUtility.mjs";
 import { userRouter } from "#routers/api/v1/userRouter.mjs";
-import { csrfErrorHandlerMiddleware } from "#middlewares/csrfErrorHandlerMiddleware.mjs";
+import { jwtStrategy } from "#strategies/jwtStrategy.mjs";
+import { isProduction } from "#utilities/envUtility.mjs";
+import { logger } from "#utilities/loggerUtility.mjs";
 
 const app = express();
 
@@ -37,7 +37,6 @@ passport.use(jwtStrategy);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/csrf-token", csrfRouter);
 app.use(doubleCsrfProtection);
-
 app.use("/api/v1/users", userRouter);
 
 app.use(notFoundMiddleware);
