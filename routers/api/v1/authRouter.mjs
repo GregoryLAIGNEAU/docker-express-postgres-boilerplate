5d55ea0f2a6d49b04268f46abce619ddb1d20c39
Gregory <gregory.laigneau@protonmail.com>
2025-10-03 04:09:23 +0700

    Add isGuest middleware to block logged-in users

--- routers/api/v1/authRouter.mjs
+++ routers/api/v1/authRouter.mjs
@@ -2,15 +2,17 @@ import { Router } from "express";
 import { authController } from "#controllers/index.mjs";
 import { jwtAuthMiddleware } from "#middlewares/authMiddleware.mjs";
 
+import { isGuest } from "#middlewares/authMiddleware.mjs";
+
 export const authRouter = Router();
 
-authRouter.post("/register", authController.postRegister);
+authRouter.post("/register", isGuest, authController.postRegister);
 
 authRouter.get("/register/activate", authController.getActivateAccount);
 
-authRouter.post("/register/resend-verification", authController.postResendVerification);
+authRouter.post("/register/resend-verification", isGuest, authController.postResendVerification);
 
-authRouter.post("/login", authController.postLogin);
+authRouter.post("/login", isGuest, authController.postLogin);
 
 authRouter.post("/forgot-password", authController.postForgotPassword);
 
