fc891739a5b29ecfbbba7882117f7f6c370e5072
Gregory <gregory.laigneau@protonmail.com>
2025-10-08 01:49:02 +0700

    Add time constants in milliseconds

---
+++ constants/timeConstant.mjs
@@ -0,0 +1,4 @@
+export const MS_IN_ONE_SECOND = 1000;
+export const MS_IN_ONE_MINUTE = 60 * MS_IN_ONE_SECOND;
+export const MS_IN_ONE_HOUR = 60 * MS_IN_ONE_MINUTE;
+export const MS_IN_ONE_DAY = 24 * MS_IN_ONE_HOUR;
