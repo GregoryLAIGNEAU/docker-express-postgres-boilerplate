a01c7669eb2d3afbe0385fa04c6d441e75882e60
Gregory <gregory.laigneau@protonmail.com>
2025-10-19 00:48:58 +0700

    Clean user JSON output using serializer

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -1,6 +1,7 @@
 import { ACCOUNT_STATUS } from "#constants/accountStatusConstant.mjs";
 import { NotFoundError } from "#errors/indexError.mjs";
 import { getUserById, updateUserById } from "#models/userModel.mjs";
+import { serializeUser } from "#serialiazers/userSerializer.mjs";
 import { clearAuthCookies } from "#services/authService.mjs";
 import { updateUserValidator } from "#validators/userValidator.mjs";
 
@@ -13,7 +14,9 @@ export const getUser = async (req, res) => {
     throw new NotFoundError("User not found");
   }
 
-  return res.status(200).json(user);
+  const serializedUser = serializeUser(user);
+
+  return res.status(200).json(serializedUser);
 };
 
 export const updateUser = async (req, res) => {
@@ -22,13 +25,16 @@ export const updateUser = async (req, res) => {
   const payload = await updateUserValidator.validate(req.body, {
     meta: { userId: currentUserId },
   });
+
   const updatedUser = await updateUserById(currentUserId, payload);
 
   if (!updatedUser) {
     throw new NotFoundError("User not found");
   }
 
-  return res.status(200).json(updatedUser);
+  const serializedUpdatedUser = serializeUser(updatedUser);
+
+  return res.status(200).json(serializedUpdatedUser);
 };
 
 export const deleteUser = async (req, res) => {
@@ -38,6 +44,7 @@ export const deleteUser = async (req, res) => {
     account_status_id: ACCOUNT_STATUS.deactivated,
     deleted_at: new Date(),
   };
+
   const updatedUser = await updateUserById(currentUserId, payload);
 
   if (!updatedUser) {
