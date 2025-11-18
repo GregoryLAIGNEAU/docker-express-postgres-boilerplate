848e1b743b9b1ca397e81fccd16a10b0df51a237
Gregory <gregory.laigneau@protonmail.com>
2025-11-19 03:10:19 +0700

    Remove assertEnvVarUtility after moving functions to envUtils

--- utilities/assertEnvVarUtility.mjs
+++
@@ -1,9 +0,0 @@
-export function requireEnvVar(name, parser = String) {
-  const raw = process.env[name];
-
-  if (!raw) {
-    throw new Error(`${name} is not defined in environment variables`);
-  }
-
-  return parser(raw);
-}
